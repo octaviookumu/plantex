d1d1bc408438dd20cacc5a4033294e1ecfd5ec97
Octavian Okumu <octaviookumu@gmail.com>
2021-11-06 01:02:51 +0300

    Add scroll active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,8 +75,13 @@ function scrollActive(){
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id')
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id');
+        
+        // console.log("scrollY", scrollY);
+        // console.log("sectionHeight", sectionId, sectionHeight);
+        // console.log("sectionTop", current.offsetTop, sectionTop);
+        // console.log("sectionId", sectionId);
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
